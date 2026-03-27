d46777d5d911b901b0c602397ecb5d3e55d4deff
shnwazdevvv <shnwaz.devv@gmail.com>
2026-03-27 17:22:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,8 +24,8 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 900))
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
-# Get this value from @BRANDRD_ROBOT on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", "7250012103"))
+# Get this value from @sexySenpaiBot on Telegram by /id
+OWNER_ID = int(getenv("OWNER_ID", "8206476526"))
 
 LOG = int(getenv("LOG", True))
 
